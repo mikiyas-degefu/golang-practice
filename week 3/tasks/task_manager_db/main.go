f532632c1c49ab0a3f992f482330716dd53de267
Mikiyas <mikiyas.m.degefu@gmail.com>
2025-11-19 22:04:52 +0300

    feat: encryp password

--- week 3/tasks/task_manager_db/main.go
+++ week 3/tasks/task_manager_db/main.go
@@ -7,12 +7,10 @@ import (
 )
 
 func main() {
-	// Initialize MongoDB connection. Reads MONGODB_URI and MONGODB_DB from env (defaults provided).
 	if err := data.InitDB(); err != nil {
 		log.Fatalf("failed to initialize database: %v", err)
 	}
 
 	r := router.SetupRouter()
-	// default listen on :8080
 	r.Run(":8080")
 }
---
+++ week 4/go-auth/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type User struct {
+	ID       uint   `json:"id"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+var users = make(map[string]*User)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "Welcome to the Go Authentication and Authorization tutorial!",
+		})
+	})
+
+	router.POST("/register", func(ctx *gin.Context) {
+		var user User
+
+		// parse json
+		if err := ctx.ShouldBindJSON(&user); err != nil {
+			ctx.JSON(400, gin.H{
+				"error": "Invalid request payload",
+			})
+			return
+		}
+
+		// check required fields
+		if user.Email == "" || user.Password == "" {
+			ctx.JSON(400, gin.H{
+				"error": "Email and password are required",
+			})
+			return
+		}
+
+		//check user already exist
+		if _, exists := users[user.Email]; exists {
+			ctx.JSON(400, gin.H{
+				"error": "User already exists",
+			})
+			return
+		}
+
+		//hash password
+		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+		if err != nil {
+			ctx.JSON(500, gin.H{
+				"error": "Internal server error",
+			})
+			return
+		}
+
+		user.Password = string(hashedPassword)
+
+		users[user.Email] = &user
+
+		ctx.JSON(200, gin.H{"message": "User registered successfully"})
+
+	})
+
+	router.Run()
+}
