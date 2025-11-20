6580ef1a9f7519b39368912d9f9b17bbcfdb4dea
Mikiyas <mikiyas.m.degefu@gmail.com>
2025-11-20 21:02:50 +0300

    feat: jwt done

--- week 4/go-auth/main.go
+++ week 4/go-auth/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 	"golang.org/x/crypto/bcrypt"
@@ -16,8 +19,49 @@ var users = make(map[string]*User)
 
 var jwtSecret = []byte("mike")
 
+func AuthMiddleware() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		authHeader := ctx.GetHeader("Authorization")
+
+		if authHeader == "" {
+			ctx.JSON(401, gin.H{"error": "Authorization header is required"})
+			ctx.Abort()
+			return
+		}
+
+		authParts := strings.Split(authHeader, " ")
+		if len(authParts) != 2 || strings.ToLower(authParts[0]) != "bearer" {
+			ctx.JSON(401, gin.H{"error": "Invalid authorization header"})
+			ctx.Abort()
+			return
+		}
+
+		token, err := jwt.Parse(authParts[1], func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+			}
+			return jwtSecret, nil
+		})
+
+		if err != nil || !token.Valid {
+			ctx.JSON(401, gin.H{"error": "Invalid JWT"})
+			ctx.Abort()
+			return
+		}
+
+		if claims, ok := token.Claims.(jwt.MapClaims); ok {
+			ctx.Set("user", claims)
+		}
+
+		ctx.Next()
+
+	}
+}
+
 func main() {
 	router := gin.Default()
+	protected := router.Group("/api")
+	protected.Use(AuthMiddleware())
 
 	router.GET("/", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{
@@ -102,6 +146,16 @@ func main() {
 		}
 
 		ctx.JSON(200, gin.H{"message": "User logged in successfully", "token": jwtToken})
+	})
+
+	protected.GET("/profile", func(ctx *gin.Context) {
+		userClaims := ctx.MustGet(("user"))
+
+		ctx.JSON(200, gin.H{
+			"message": "Protected route!",
+			"user":    userClaims,
+		})
+
 	})
 
 	router.Run()
